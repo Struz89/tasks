b78568be9fcc69fab9a96ca80f5f7b7a80879326
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-07-16 12:59:14 +0200

    19.3 Zadanie: dodanie maila do wiadomości (ang. CC)

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -8,6 +8,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -31,7 +33,10 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        mailMessage.setCc(mail.getToCc());
+        Optional<String> optionalToCc = Optional.ofNullable(mail.getToCc());
+        if (optionalToCc.isPresent()) {
+            mailMessage.setCc(mail.getToCc());
+        }
         return mailMessage;
     }
 }
