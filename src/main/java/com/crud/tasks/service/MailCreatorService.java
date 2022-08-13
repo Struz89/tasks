1d256866ab4a3b805bf14dd74910a56d3fb2e198
pstruzyn <pawel.struzynski@pentacomp.pl>
2022-08-13 20:32:26 +0200

    24.3 Zadanie: Wykonaj szablon maila

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -29,4 +32,24 @@ public class MailCreatorService {
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
+    public String buildTrelloTaskEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("preview", "Trello - Kodilla Application");
+        context.setVariable("goodbye_message", "Best regards :) Kodilla");
+        context.setVariable("company_details", adminConfig.getAdminMail());
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-task-mail", context);
+    }
 }
